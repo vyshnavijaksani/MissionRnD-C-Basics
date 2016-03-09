618b84a5494e12bc5393f7f49aa4bc96c9e3ea41
vyshnavijaksani <vyshnavijaksani@missionrnd.com>
2016-03-09 19:34:56 +0530

    Submit Commit

--- src/count.cpp
+++ src/count.cpp
@@ -12,5 +12,13 @@ ERROR CASES: Return -1 in error cases.
 NOTES:
 */
 int count(int num1, int num2) {
-	return 0;
+	int i, counter=0;
+	for (i = num1; i <= num2; i++)
+	{
+		if (i%num1 == 0)
+		{
+			counter++;
+		}
+	}
+	return counter;
 }
--- src/swap.cpp
+++ src/swap.cpp
@@ -13,5 +13,8 @@ NOTES:
 
 void swap(int *a, int *b)
 {
-	
+	int temp=0;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
